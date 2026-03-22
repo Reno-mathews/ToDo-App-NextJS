48b0c29474ffb4ebba7e2041cdb3907a214f4d54
Reno-mathews <renomathews10pro@gmail.com>
2026-03-22 11:49:38 +0530

    Setting up

--- nextjs-todo/app/page.tsx
+++ nextjs-todo/app/page.tsx
@@ -10,5 +10,17 @@ type Task = {
 export default function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [title, setTitle] = useState("");
+
+  const fetchTasks = async() => {
+    const res = await fetch("/api/tasks");
+    const data = await res.json();
+    setTasks(data);
+  };
+
+  const addTask = async() => {
+    if (!title) return;
+
+    
+  }
 }
 
